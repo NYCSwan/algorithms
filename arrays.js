91f5bd077d0a641717cad2fd83d927bb6e6b92b7
NYCSwan <megan.swanby@gmail.com>
2017-01-12 12:28:14 -0500

    Updated ES6 quick sort function

--- arrays.js
+++ arrays.js
@@ -161,6 +161,24 @@ function quickSort(unsortedArr){
   return quickSort(left).concat(pivot, quickSort(right));
 }
 
+// ES6
+
+const quickSort = nums => {
+	if(nums.length <=1) return nums;
+
+	const pivot = nums[nums.length-1];
+	const left = [];
+	const right = [];
+
+	for(var i=0; i < nums.length-1; i++) {
+		if(nums[i]< pivot) {
+			left.push(nums[i]);
+		} else {
+			right.push(nums[i]);
+		}
+	}
+	return [...quickSort(left), pivot, ...quickSort(right)];
+}
 
 
 // unit tests
