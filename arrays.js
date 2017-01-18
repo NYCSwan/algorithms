f94e344ee8a57b37ea1e721cd402f6593807ce90
NYCSwan <megan.swanby@gmail.com>
2017-01-18 18:48:39 -0500

    Add ES6 quicksort function

--- arrays.js
+++ arrays.js
@@ -167,8 +167,8 @@ const quickSort = nums => {
 	if(nums.length <=1) return nums;
 
 	const pivot = nums[nums.length-1];
-	const left = [];
-	const right = [];
+	let left = [];
+	let right = [];
 
 	for(var i=0; i < nums.length-1; i++) {
 		if(nums[i]< pivot) {
