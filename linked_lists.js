739d6e439fc0a009c3330958a82df310b276e0fb
NYCSwan <megan.swanby@gmail.com>
2017-01-19 08:58:52 -0500

    Added linkedLists functions

--- linked_lists.js
+++ linked_lists.js
@@ -50,28 +50,58 @@ function linkedList() {
 
 		push(value) {
 			const node = new Node(value);
-			this.length++;
+			this.legnth++;
 			if (!this.head) { // is head null?
-			 this.tail = node;
+			  this.tail = node;
 			} else {
-				this.tail.next = node;
+			  this.tail.next = node;
 			}
 			this.tail = node;
 		}
 		pop() {
-
-		}
-		_find(comparator, test=this._test) {
-
-		}
-		_test(a,b){ // _ means internal method, convention
-		  return a===b;
+			this.delete(this.length-1);
 		}
 		get(index){
+		 const node = this._find(index, this._testIndex); 
+		 if (!node) return null;
+		 return node.value;
 
 		}
 		delete(index){
-
+			if (index===0) {  //special case if have to delete the heed
+				const head = this.head;
+				if(head) {
+					this.head = head.next
+				} else {
+					this.head = null;
+				}
+				this.length--;
+				return head.value;
+			}
+			const node = this._find(index-1, this._testIndex) //looking for the thing before the index to reassign the next.
+			const excise = node.next; //what actually gets deleted
+			if (!excise) return null;
+			node.next = excise.next; //node's pointer before deleted thing becomes the deteled thing's pointer
+			if (node.next && !node.next.next) {// if there is a node in the next position but nothing after the deleted thing
+				this.tail = this.next;
+			}
+			this.length--;
+			return excise.value;
+		}
+		_find(comparator, test=this._test) {
+		  let current = this.head;
+		  let i = 0;
+		  while(current) {
+		   	if (test(value, current.value, current)){ //looking for, val of node I'm looking at, node itself
+		   	  return current;
+		   } 
+		   current = current.next;
+		   i++;
+		  }
+		  return null;
+		}
+		_test(a,b){ // _ means internal method, convention
+		  return a===b;
 		}
 		testIndex(search, __, i) { //__ mean I don't care what it is
 			return search === i;
